f1d528ae061bc2735ca949fb4936dc375db6ff48
colelc <colelc@gmail.com>
2024-01-29 15:30:56 -0500

    commit

--- src/app/new-acronym/new-acronym.component.ts
+++ src/app/new-acronym/new-acronym.component.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AcronymsAdminViewComponent } from '../acronyms-admin-view/acronyms-admin-view.component';
+import { Acronym } from '../interface/acronym-if';
 
 @Component({
   selector: 'app-new-acronym',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-acronym.component.html',
   styleUrl: './new-acronym.component.css'
 })
 export class NewAcronymComponent implements OnInit {
+  // private acronyms: Acronym[] =  [
+  acronym: Acronym;
+
+  constructor() {
+    this.acronym = {
+        id: 35, 
+        acronym: "", 
+        refersTo: "", 
+        definition: "",
+        areaKey: "",
+        active: true,
+        tags: [],
+        tagString: "",
+        createdBy: "",
+        created: "",
+        lastUpdatedBy: "",
+        lastUpdated: ""
+    }
+  }
 
   ngOnInit(): void {
     console.log("NewAcronym ngOnInit");
