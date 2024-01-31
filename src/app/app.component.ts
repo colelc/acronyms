8adce9bbe0510c5b68e224328950c8bf8f4a6515
colelc <colelc@gmail.com>
2024-01-31 07:55:42 -0500

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
-//import { AcronymsHeaderComponent } from './acronyms-header/acronyms-header.component';
-import { AcronymsListComponent } from './acronyms-list/acronyms-list.component';
+//import { AcronymsListComponent } from './acronyms-list/acronyms-list.component';
 import { AcronymsEditComponent } from './acronyms-edit/acronyms-edit.component';
 import { AcronymsFooterComponent } from './acronyms-footer/acronyms-footer.component';
 import { AcronymsAdminViewComponent } from './acronyms-admin-view/acronyms-admin-view.component';
 import { AcronymsUserViewComponent } from './acronyms-user-view/acronyms-user-view.component';
+import { ConfigService } from './service/config.service';
 import { AcronymsService } from './service/acronyms.service';
 import { UserService } from './service/user.service';
 import { HttpService } from './service/http.service';
@@ -14,21 +15,21 @@ import { HttpService } from './service/http.service';
 import { Router } from '@angular/router'
 import { RouterOutlet } from '@angular/router';
 import { RouterLink } from '@angular/router';
-import { CrisisListComponent } from './crisis-list/crisis-list.component';
-import { HeroesListComponent } from './heroes-list/heroes-list.component';
+//import { CrisisListComponent } from './crisis-list/crisis-list.component';
+//import { HeroesListComponent } from './heroes-list/heroes-list.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [ 
-    AcronymsListComponent, AcronymsFooterComponent, AcronymsEditComponent,
+    AcronymsFooterComponent, AcronymsEditComponent,
     AcronymsAdminViewComponent, AcronymsUserViewComponent
+    , HttpClientModule
     , FormsModule
     ,RouterOutlet
     , RouterLink
-    , HeroesListComponent, CrisisListComponent
   ],
-  providers: [AcronymsService, UserService, HttpService, Router],
+  providers: [AcronymsService, UserService, HttpService, ConfigService, Router],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   schemas: [/*CUSTOM_ELEMENTS_SCHEMA*/] // need this for PrimeNg to work
--- src/app/service/acronyms.service.ts
+++ src/app/service/acronyms.service.ts
@@ -1,132 +1,66 @@
-import { Injectable } from "@angular/core";
+import { Injectable, OnInit } from "@angular/core";
 import { Acronym } from "../interface/acronym-if";
 import { Tag } from "../interface/tag-if";
 import { HttpService } from "./http.service";
+import { Observable } from "rxjs";
 
 @Injectable({providedIn: "root"})
-export class AcronymsService {
+export class AcronymsService implements OnInit {
 
-    private acronyms: Acronym[] =  [
-        {
-          id: 35, 
-          acronym: "BDW", 
-          refersTo: "Blue Devil Weekend", 
-          definition: "Weekend-long event for admitted students, organized by the Office of Admissions and student volunteers. There is a BDW in February and again in April to accommodate all admitted studentsâ€™ timelines. Admissions also hosts several other weekend events for prospective Daytime students in Fall. Also, the admitted studentsâ€™ weekend for the MMS and MQM programs is called Blue Devil Experience and for the EMBA programs is called Blue Devil Celebration.", 
-          areaKey: "Admissions",
-          active: true,
-          tags: [
-                {id: 1, tag: "Admissions", active: true, 
-                createdBy: "tagCBy", created: "tagC", lastUpdatedBy: "tagLuBy", lastUpdated: "tagLu"},
-                {id: 2, tag: "Tag 2", active: true, 
-                createdBy: "tagCBy", created: "tagC", lastUpdatedBy: "tagLuBy", lastUpdated: "tagLu"}
-            ],
-          tagString: "Tag 1, Tag 2",
-          createdBy: "createdBy",
-          created: "created",
-          lastUpdatedBy: "lastUpdatedBy",
-          lastUpdated: "lastUpdated"
-        },
-        {
-          id: 72, 
-          acronym: "DEF", 
-          refersTo: "refersTo", 
-          definition: "definition", 
-          areaKey: "areaKey",
-          active: true,
-          tags: [
-                {id: 1, tag: "Tag 1", active: true, 
-                createdBy: "tagCBy", created: "tagC", lastUpdatedBy: "tagLuBy", lastUpdated: "tagLu"},
-                {id: 2, tag: "Tag 2", active: true, 
-                createdBy: "tagCBy", created: "tagC", lastUpdatedBy: "tagLuBy", lastUpdated: "tagLu"}
-            ],
-          tagString: "Tag 1, Tag 2",
-          createdBy: "createdBy",
-          created: "created",
-          lastUpdatedBy: "lastUpdatedBy",
-          lastUpdated: "lastUpdated"
-        },
-        {
-          id: 21, 
-          acronym: "FANFL", 
-          refersTo: "Financial Analysis for Non-Finance Leaders", 
-          definition: "This class introduces students to the essential elements of financial reporting, including managerial and financial accounting, and helps them develop a greater understanding of how financial information is used as a strategic tool for decision making.", 
-          areaKey: "Exec Ed",
-          active: true,
-          tags: [
-                {id: 1, tag: "Exec Ed", active: true, 
-                createdBy: "tagCBy", created: "tagC", lastUpdatedBy: "tagLuBy", lastUpdated: "tagLu"},
-                {id: 2, tag: "Tag 2", active: true, 
-                createdBy: "tagCBy", created: "tagC", lastUpdatedBy: "tagLuBy", lastUpdated: "tagLu"}
-            ],
-          tagString: "Tag 1, Tag 2",
-          createdBy: "createdBy",
-          created: "created",
-          lastUpdatedBy: "lastUpdatedBy",
-          lastUpdated: "lastUpdated"
-        }
-    ];
+  private acronyms: Acronym[] =  [ ];
 
-    constructor(private httpService: HttpService) {
+  constructor(private httpService: HttpService) {
 
-    }
+  }
 
-    getAcronyms = () => {
-        // by slicing, this method returns a new array which will always have
-        //  the exact copy of the acronyms array within this class
-        return this.acronyms.slice();
+  ngOnInit() {}
 
-        // WE WILL WANT TO GO TO THE SERVER HERE, TO GET A NEW ACRONYMS LIST
-    }
+  getAcronyms = () => {
+      // by slicing, this method returns a new array which will always have
+      //  the exact copy of the acronyms array within this class
+      //return this.acronyms.slice();
 
-    getAcronymById = (id: number) => {
-        for (let a of this.acronyms) {
-            if (a.id === id) {
-              return a;
-            }
-        }
-        return null;
-    }
+      // WE WILL WANT TO GO TO THE SERVER HERE, TO GET A NEW ACRONYMS LIST
+      return this.httpService.getAcronyms();
+  }
 
-    setAcronym = (id: number, acronym: string) => {
-        console.log("setAcronym: id = " + id + " acronym = " + acronym);
-    }
-
-    initAcronym = () => {
-      return {
-        id: 0, 
-        acronym: "", 
-        refersTo: "", 
-        definition: "",
-        areaKey: "",
-        active: true,
-        tags: [],
-        tagString: "",
-        createdBy: "",
-        created: "",
-        lastUpdatedBy: "",
-        lastUpdated: ""
+  getAcronymById = (id: number) => {
+      for (let a of this.acronyms) {
+          if (a.id === id) {
+            return a;
+          }
       }
+      return null;
+  }
+
+  setAcronym = (id: number, acronym: string) => {
+      console.log("setAcronym: id = " + id + " acronym = " + acronym);
+  }
+
+  initAcronym = () => {
+    return {
+      id: 0, 
+      acronym: "", 
+      refersTo: "", 
+      definition: "",
+      areaKey: "",
+      active: true,
+      tags: [],
+      tagString: "",
+      createdBy: "",
+      created: "",
+      lastUpdatedBy: "",
+      lastUpdated: ""
     }
+  }
+
+  addAcronym = (newAcronym:Acronym) => {
+    console.log("addAcronym: ", newAcronym);
+    this.httpService.addAcronym(newAcronym);
+  }
 
     // initializeNewAcronym = () => {
     //   console.log("initialize new acronym");
-    //   const a = {} as Acronym;
-    //   a.id = 0;
-    //   a.acronym = "";
-    //   a.refersTo = "Refers To";
-    //   a.definition = "Definition";
-    //   a.areaKey = "Area Key";
-    //   a.active = true;
-
-    //   const t = {} as Tag;
-    //   a.tags = [t];
-    //   a.tagString = "";
-      
-    //   a.created = "";
-    //   a.createdBy = "";
-    //   a.lastUpdated = "";
-    //   a.lastUpdatedBy = "";
-
     //   const newAcronymList: Acronym[] = [];
     //   newAcronymList.push(a);
 
@@ -141,9 +75,6 @@ export class AcronymsService {
     //   }
     // }
 
-    addAcronym = (newAcronym:Acronym) => {
-      console.log("addAcronym: ", newAcronym);
-      this.httpService.addAcronym(newAcronym);
-    }
+
 
 }
